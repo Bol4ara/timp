7b4adadb46d0279e06a5795640bb202f4f33b18b
Bol4ara <85251521+Bol4ara@users.noreply.github.com>
2021-06-05 11:09:10 +0700

    Delete pr1 directory

--- pr1/03_pow.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <math.h>
-
-int main() {
-    double a, b;
-    (void)scanf("%lf %lf", &a, &b);
-    printf("%lf\n", pow(a, b));
-    return 0;
-}
--- pr1/tests/01_hello_world.py
+++
@@ -1,5 +0,0 @@
-import os
-
-actual = os.popen('./a.out').read().strip()
-expected = 'Hello, World!'
-assert actual == expected, "'{}' != {}".format(actual, expected)
